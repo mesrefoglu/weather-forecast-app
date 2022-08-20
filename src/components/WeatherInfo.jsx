bc53c26872c36f665d3d5bdc90ead79c1a815f98
Qedized <qedized@gmail.com>
2022-08-20 00:24:49 -0400

    Update WeatherInfo.jsx
    Adjusted and made it a little wider

--- src/components/WeatherInfo.jsx
+++ src/components/WeatherInfo.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import {
-  UilRegistered,
   UilArrowUp,
   UilArrowDown,
   UilTemperature,
@@ -19,7 +18,7 @@ function WeatherInfo() {
         <p>Sunny</p>
       </div>
       <div className="flex items-center justify-center">
-        <div className="flex flex-row items-center justify-between w-3/4 text-white py-3">
+        <div className="flex flex-row items-center justify-between w-11/12 text-white py-3">
           <img
             src="https://img.icons8.com/color/48/000000/sun.png"
             alt="sun"
@@ -45,30 +44,32 @@ function WeatherInfo() {
           </div>
         </div>
       </div>
-      <div className="flex flex-row items-center justify-center text-white text-sm py-3">
-        <div className="flex flex-row items-center space-x-2 justify-center mx-4">
-          <UilSun />
-          <p className="font-light">
-            Rise: <span className="font-medium ml-1">06:45 am</span>
-          </p>
-        </div>
-        <div className="flex flex-row items-center space-x-2 justify-center mx-4">
-          <UilSunset />
-          <p className="font-light">
-            Set: <span className="font-medium ml-1">08:45 pm</span>
-          </p>
-        </div>
-        <div className="flex flex-row items-center space-x-2 justify-center mx-4">
-          <UilTemperature />
-          <p className="font-light">
-            High: <span className="font-medium ml-1">30&deg;</span>
-          </p>
-        </div>
-        <div className="flex flex-row items-center space-x-2 justify-center mx-4">
-          <UilTemperatureEmpty />
-          <p className="font-light">
-            Low: <span className="font-medium ml-1">10&deg;</span>
-          </p>
+      <div className="flex items-center justify-center">
+        <div className="flex flex-row items-center justify-between w-5/6 ml-9 text-white text-sm py-3">
+          <div className="flex flex-row items-center space-x-2 justify-center">
+            <UilSun />
+            <p className="font-light">
+              Rise: <span className="font-medium ml-1">06:45 am</span>
+            </p>
+          </div>
+          <div className="flex flex-row items-center space-x-2 justify-center">
+            <UilSunset />
+            <p className="font-light">
+              Set: <span className="font-medium ml-1">08:45 pm</span>
+            </p>
+          </div>
+          <div className="flex flex-row items-center space-x-2 justify-center">
+            <UilTemperature />
+            <p className="font-light">
+              High: <span className="font-medium ml-1">30&deg;</span>
+            </p>
+          </div>
+          <div className="flex flex-row items-center space-x-2 justify-center">
+            <UilTemperatureEmpty />
+            <p className="font-light">
+              Low: <span className="font-medium ml-1">10&deg;</span>
+            </p>
+          </div>
         </div>
       </div>
     </div>
