0a10f3daeba5c6ad9c755be0ac955a85bcde4630
Qedized <qedized@gmail.com>
2022-09-13 21:58:37 -0400

    Added flag emoji
    Beside the country name, now it displays the flag.

--- src/components/LocationAndTime.jsx
+++ src/components/LocationAndTime.jsx
@@ -4,12 +4,21 @@ import { getDateTime } from "../services/weatherService";
 
 const regionNames = new Intl.DisplayNames(["en"], { type: "region" });
 
+// https://dev.to/jorik/country-code-to-flag-emoji-a21
+function getFlagEmoji(countryCode) {
+  const codePoints = countryCode
+    .toUpperCase()
+    .split("")
+    .map((char) => 127397 + char.charCodeAt());
+  return String.fromCodePoint(...codePoints);
+}
+
 function LocationAndTime({ weather: { timezone, city, country } }) {
   return (
     <div>
       <div className="flex items-center justify-center my-6">
         <p className="text-white text-3xl font-medium">
-          {city}, {regionNames.of(country)}
+          {city}, {regionNames.of(country)} {getFlagEmoji(country)}
         </p>
       </div>
       <div className="flex flex-row items-center justify-center my-2">
